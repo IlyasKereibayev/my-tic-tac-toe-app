27da03a39ee71fa8876ad66c22dd4e829aa06db3
Ilyas Kereibayev <ilyaskereibayev@gmail.com>
2020-12-04 23:24:53 +0600

    ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import GameStatus from "./GameStatus";
+import GameBoard from "./GameBoard";
 
-function App() {
+const App = () => {
+	const [winner, setWinner] = useState(null)
+  const [currentMove, setCurrentMove] = useState('X')
+  const [boardState, setBoardState] = useState(new Array(9).fill(null))
+	const toggleMove = () => {
+  	setCurrentMove(currentMove === 'X' ? 'O' : 'X')
+	}
+	const handleClick = (index) => {
+		if(winner) {
+			return;
+		}
+ 		const newBoardState = boardState.map((item, i) => {
+ 			if(index === i) {
+ 				item = currentMove
+			}
+ 			return item
+		})
+		
+		
+		let winnerFound = findWinner(newBoardState)
+		if(winnerFound) {
+			setBoardState(newBoardState)
+			setWinner(winnerFound)
+		} else {
+			if(isGameFinished(newBoardState)) {
+				setBoardState(newBoardState)
+				setWinner('draw')
+			} else {
+				setBoardState(newBoardState)
+				toggleMove()
+			}
+		}
+	}
+	const winLines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	]
+	const findWinner = (boardState) => {
+		for(let i=0; i<winLines.length; i++) {
+			let line = winLines[i]
+			if(boardState[line[0]]
+					&& boardState[line[0]] === boardState[line[1]]
+						&& boardState[line[1]] === boardState[line[2]]) {
+				return boardState[line[0]]
+			}
+		}
+		return null
+	}
+	const isGameFinished = (boardState) => {
+		return boardState.every((item) => !!item)
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="container-fluid">
+	  <div className="wrapper">
+		<header>
+		  <h1>Tic Tac Toe game</h1>
+		</header>
+		<div className="game">
+		  <GameStatus currentMove={currentMove} winner={winner}/>
+		  <GameBoard boardState={boardState} handleClick={handleClick}/>
+		</div>
+	  </div>
+	
+	</div>
   );
 }
 
